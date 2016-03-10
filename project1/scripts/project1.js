92a42102408b23ea8c597017cab19c92f9da7b01
Natalia Larina <koshka_208@yahoo.com>
2016-03-10 15:08:39 -0600

    changes

--- project1/scripts/project1.js
+++ project1/scripts/project1.js
@@ -1,64 +1,4 @@
-//beginning of document, when the page loads, what information is ready to use
-$(document).ready(function(){
-//countries are loaded from json
-  var countries = {
-    type: 'get',
-    dataType: 'json',
-    url: 'https://restcountries.eu/rest/v1/all',
-    success: function(countries) {
-    //if success go through each country in json and push country and capital in myCountries array
-      countries.forEach(function(country, i) {
-        myCountries.push(new Country(country.name, country.capital))
-      });
-    },
-//if not you will see error alert on the screen
-    error: function(error) {
-      alert(error);
-    }
-}
-
- $.ajax(countries).done(function(data) {
-    // when ajax is working and we got the info, when .play(Let'sPlay) button is clicked the new game
-    //will start
-    $('.play').click(function(){
-// start new game function
-      // startNewGameRound();
-      //empty out country p/ make sure it is empty before the start of the game
-      $('.country').empty()
-      //if it is empty console.log empty
-      console.log("empty")
-      //insert new country name
-      $('.country').text(randomCountry());
-      $('.answers').empty();
-      answerArray.push(createButton(country.capital));
-      console.log(country.capital)
-      // var buttons = []
-
-      return appendArray()
-    });
-
-    // next.click
-    $('.next').click(function() {
-      // change the country first before these 2 lines
-      $('.country').empty().text(country.name);
-      startNewGameRound();
-    });
-
-    // answersContainer = $('.answers');
-    // appendArray(answersContainer);
-
-    function randomIndex () {
-       Math.round(Math.random() * 4)
-    }
-    var score = 0;
-    var correct = 0;
-    var wrong = 0;
-    function countryChosen(){
-      return $('.country').text();
-    }
-  }); // ajax.done is finished
-// }); // end document.ready
-
+console.log('js is reading the page right now');
 var answerArray = []; //array where answers/capitals are pushed, 1 correct 3 wrong
 var myCountries = []; //array where objects (countries and capitals) are pushed from Json
 
@@ -73,13 +13,15 @@ var round = 0; //how many questions in one game
 
 //constructor for each country
 function Country(name, capital) {
+    console.log('new Country()');
     this.name = name;
     this.capital = capital;
-}
+}//end of constructor
 
 function createButton(country) {
+  console.log('createButton');
   var button = document.createElement("button");
-  button.innerHTML = country;
+  button.innerHTML = country.name;
   button.addEventListener("click", function(){
     console.log('btn is working')
     if ($(this).text() === compare(myCountries)){
@@ -92,9 +34,10 @@ function createButton(country) {
     }
   }, false);
   return button;
-}
+}//end of createButton
 
 function startNewGameRound() {
+  console.log('startNewGameRound()')
   $('.country').empty()
   country = randomCountry();
   console.log(country);
@@ -104,9 +47,10 @@ function startNewGameRound() {
 //   }
 //   round++;
 //   // ('.answers').append(());
-}
+}//end of startNewGameRound
 
 function shuffleArray(array) {
+  console.log('shuffleArray');
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
@@ -114,48 +58,60 @@ function shuffleArray(array) {
         array[j] = temp;
     }
     return array;
-}
+}//end of shuffle array
+
 
 function randomCountry() {
+  console.log('randomCountry');
   var countrySelected = myCountries[Math.floor(Math.random() * myCountries.length)];
   return countrySelected;
-};
+};//end of randomCountry
+
 
 function appendArray(element){
-  for (var i = 0; i < 3; i++) {
+  console.log('appendArray');
+  answerArray = [];
+  for (var i = 0; i < 5; i++) {
     var randomCtry = randomCountry();
     answerArray.push(createButton(randomCapital()));
   }
+
   var myArray = shuffleArray(answerArray);
 
   for (var i = 0; i < 4; i++){
-    $(element).append(myArray[i])
+    $(element).append(myArray[i]);
   }
-  answerArray.length = 0;
-}
+  // answerArray.length = 0;
+  return answerArray;
+}//end of appendArray
 
-function randomCapital () {
+function randomCapital() {
+  console.log('randomCapital');
   var capitalSelected = myCountries[Math.floor(Math.random() * myCountries.length)];
   console.log(capitalSelected)
-  return JSON.stringify(capitalSelected);
-};
+  return capitalSelected;
+};//end of randomCapital
 
 function scoreboard(){
+  console.log('scoreboard()');
   score++
   $('.score').text(score)
-}
+}//end of scoreboard
 
 function correctboard(){
+  console.log('correctboard()');
   correct++
   $('.correct').text(correct)
-}
+}//end of correctboard
 
 function wrongboard(){
+  console.log('wrongBoard()');
   wrong++;
   $('.wrong').text(wrong)
-}
+}//end of wrongboard
 
 function compare(countries){
+  console.log('compare()');
   //loop
   for (var i = 0; i < myCountries.length; i++) {
     if (countryChosen() == countries[i].name) {
@@ -163,60 +119,18 @@ function compare(countries){
     }
  }
 }//end of function compare
-//beginning of document
-$(document).ready(function(){
-  var countries = {
-    type: 'get',
-    dataType: 'json',
-    url: 'https://restcountries.eu/rest/v1/all',
-    success: function(countries) {
-      countries.forEach(function(country, i) {
-        myCountries.push(new Country(country.name, country.capital))
-      });
-      //console.log(countries)
-    },
-    error: function(error) {
-      alert(error);
-    }
-  };
-  $.ajax(countries)
-  //
-  // $.ajax(countries).done(function(data) {
-  //   // play.click
-  //   $('.play').click(function(){
-  //     startNewGameRound();
-  //     $('.country').empty()
-  //     console.log(country)
-  //     $('.country').text(country.name);
-  //     $('.answers').empty();
-  //     answerArray.push(createButton(country.capital));
-  //     console.log(country.capital)
-  //     var buttons = []
-  //
-  //     return appendArray()
-  //   });
 
-    // next.click
-    $('.next').click(function() {
-      // change the country first before these 2 lines
-      $('.country').empty().text(country.name);
-      startNewGameRound();
-    });
+function randomIndex () {
+  console.log('randomIndex');
+   Math.round(Math.random() * 4)
+}//end of function rendomIndex
 
-    answersContainer = $('.answers');
-    appendArray(answersContainer);
+function countryChosen(){
+  console.log('countryChosen()');
+  return $('.country').text();
+}//end of countryChosen
 
-    function randomIndex () {
-       Math.round(Math.random() * 4)
-    }
-    // var score = 0;
-    // var correct = 0;
-    // var wrong = 0;
-    function countryChosen(){
-      return $('.country').text();
-    }
-  }); // ajax.done is finished
-}); // end document.ready
+
 
 // TIMER: SOON
 // start timer - https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setInterval
@@ -240,3 +154,68 @@ $(document).ready(function(){
 //setInterval(function, howLongBetweenFuntionCalls)
 // var timer = setInterval(everySecond, oneSecondInMilliseconds);
 // end timer
+
+
+
+//beginning of document, when the page loads, what information is ready to use
+$(document).ready(function(){
+  console.log('document.ready');
+//countries are loaded from json
+  var countries = {
+    type: 'get',
+    dataType: 'json',
+    url: 'https://restcountries.eu/rest/v1/all',
+    success: function(countries) {
+      console.log('ajax finished');
+
+    //if success go through each country in json and push country and capital in myCountries array
+      countries.forEach(function(country, i) {
+        myCountries.push(new Country(country.name, country.capital))
+      });
+    },
+//if not you will see error alert on the screen
+    error: function(error) {
+      alert(error);
+    }
+}
+
+ $.ajax(countries).done(function(data) {
+    // when ajax is working and we got the info, when .play(Let'sPlay) button is clicked the new game
+    //will start
+    $('.play').click(function(){
+      console.log('.play click');
+      //when let's play is clicked:
+      //empty out country p/ make sure it is empty before the start of the game
+      $('.country').empty()
+      //if it is empty console.log empty
+      var newCountry = randomCountry();
+      //gives new country on the screen
+      $('.country').text(newCountry.name);
+      //give new array of answers here:
+
+      $('.answers').empty();
+      appendArray($('.answers'));
+      //answerArray.push(appendArray(answerArray));
+      console.log("answerArray")
+
+
+      // return appendArray()
+      // console.log("appendArray");
+    });
+
+    // next.click
+    $('.next').click(function() {
+      console.log('.next click');
+    //gives new country on the screen
+      $('.country').empty();
+      console.log('empty');
+      var newCountry = randomCountry();
+      $('.country').text(newCountry.name);
+      //give new array of answers
+      //???????????????????????
+    });
+
+
+
+  }); // ajax.done is finished
+}); // end document.ready
