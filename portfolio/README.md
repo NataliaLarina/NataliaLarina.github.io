c6470a9dba360bd9ea6730158f67b0cfd57a4b73
Natalia Larina <koshka_208@yahoo.com>
2016-05-10 10:56:58 -0500

    portfolio

---
+++ portfolio/README.md
@@ -0,0 +1,11 @@
+![screenshot.png](images/portfolio.png)
+
+# Portfolio
+
+### Objective
+Portfolio about me and my projects for people to see who I am & what I made. 
+
+### Technologies Used
+- HTML5
+- CSS
+- Javascript/jQuery
---
+++ portfolio/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log('lol')
+
+  $('#carousel-example-generic').carousel({
+    interval: 2000
+   });
+
+});
+
+$('.flip').click(function(){
+
+  $(this).find('.card').toggleClass('flipped');
+
+});
